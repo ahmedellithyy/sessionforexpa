e7bd2787d3136637e22a93306b7699aecd4328ba
Lithium <ellithy12344321@gmail.com>
2024-05-12 13:53:46 +0200

    Update dataextractionfunction.js

--- dataextractionfunction.js
+++ dataextractionfunction.js
@@ -1,4 +1,4 @@
-function dataExtraction(graphql)
+function dataExtraction(graphql) //don't forget to pass the query from the other function
 {
   
   var requestOptions = {
@@ -6,10 +6,10 @@ function dataExtraction(graphql)
     'payload': graphql,
     'contentType':'application/json',
     'headers':{
-      'access_token': "ACCESS_TOKEN"
+      'access_token': "ACCESS_TOKEN" //replace ACCESS_TOKEN with the token you have it
     }
   };
   var response = UrlFetchApp.fetch(`https://gis-api.aiesec.org/graphql?access_token=${access_token}`, requestOptions);
   var recievedDate = JSON.parse(response.getContentText());
-  return recievedDate.data.allOpportunityApplication.data;
+  return recievedDate.data.allOpportunityApplication.data; //change here allOpportunityApplication based on the data you are requesting (people...etc.)
 }
